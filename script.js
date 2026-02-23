18d7ae27d5e2a1f204bd54a716c19991b0bc54ac
Aflah A <98900571+itsaflah@users.noreply.github.com>
2026-02-23 12:22:36 +0530

    custom domain connected

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ function includeHTML(file, elementId) {
         .then(data => document.getElementById(elementId).innerHTML = data);
 }
 
-includeHTML('/ppmhsskottukkara/common/navbar.html', 'navbar-placeholder');
-includeHTML('/ppmhsskottukkara/common/footer.html', 'footer-placeholder');
+includeHTML('/common/navbar.html', 'navbar-placeholder');
+includeHTML('/common/footer.html', 'footer-placeholder');
 
 // Navbar functionality
 function initNavbar() {
